0dbc94a7788ef8e2f1497d4f6c991a9aaddad84d
iamashwin <ashwin-kumar.karnad@mpsd.mpg.de>
2023-02-06 16:15:24 +0100

    implement discover and upload template

--- apps/Discover.py
+++ apps/Discover.py
@@ -1,4 +1,11 @@
 import streamlit as st
-
+import shelve
 def app():
-    pass
+    if "address" in st.session_state:
+        shelvedb = shelve.open("data.db")
+        for key in shelvedb:
+            st.write(f"## {shelvedb[key]['title']}")
+            st.write(f"### {shelvedb[key]['author']}")
+            st.write(f"### {shelvedb[key]['release_date']}")
+            st.image(shelvedb[key]['cover'])
+            st.audio(shelvedb[key]['music'])
--- apps/Upload.py
+++ apps/Upload.py
@@ -1,4 +1,24 @@
 import streamlit as st
+import shelve
+import time
+
 
 def app():
-    pass
+    st.write("# Upload Your own Music!")
+    st.write(
+        "Upload your own music to the MiDiFy platform and start earning money from your music!")
+    music = st.file_uploader("Upload your music here", type=["mid"])
+    author = st.text_input("Author Name")
+    title = st.text_input("Title")
+    release_date = st.date_input("Release Date")
+    st.write("## Upload your cover art")
+    cover = st.file_uploader("Upload your cover art here", type=[
+                             "png", "jpg", "jpeg"])
+    upload = st.button("Upload")
+    if upload:
+        with st.spinner("Uploading data ..."):
+            time.sleep(2)
+            shelvedb = shelve.open("data.db")
+            shelvedb[len(shelvedb)+1] = {"author": author, "title": title,
+                                         "release_date": release_date, "cover": cover, "music": music}
+            st.success("Done!")
