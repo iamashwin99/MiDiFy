c134c5e37bac308db3ad1e77b4e69cb27b954591
iamashwin <ashwin-kumar.karnad@mpsd.mpg.de>
2023-02-06 16:15:49 +0100

    update discover

--- apps/Discover.py
+++ apps/Discover.py
@@ -2,10 +2,11 @@ import streamlit as st
 import shelve
 def app():
     if "address" in st.session_state:
-        shelvedb = shelve.open("data.db")
-        for key in shelvedb:
-            st.write(f"## {shelvedb[key]['title']}")
-            st.write(f"### {shelvedb[key]['author']}")
-            st.write(f"### {shelvedb[key]['release_date']}")
-            st.image(shelvedb[key]['cover'])
-            st.audio(shelvedb[key]['music'])
+        with st.spinner("Loading data ..."):
+            shelvedb = shelve.open("data.db")
+            for key in shelvedb:
+                st.write(f"## {shelvedb[key]['title']}")
+                st.write(f"### {shelvedb[key]['author']}")
+                st.write(f"### {shelvedb[key]['release_date']}")
+                st.image(shelvedb[key]['cover'])
+                st.audio(shelvedb[key]['music'])
